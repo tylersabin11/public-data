8e55e49083e586836aa853848b6a23a59b5d0bdf
Tyler Sabin <tyler.sabin64@gmail.com>
2025-06-20 20:28:27 +0000

    Minor fixes for dup

--- models/analytics/opioid_crisis.sql
+++ models/analytics/opioid_crisis.sql
@@ -2,7 +2,8 @@ with county_names as(
     select
         opioid_id,
         state_name,
-        case when county_name is null then 'Unknown' else county_name end as county_name
+        case when county_name is null then 'Unknown' else county_name end as county_name,
+        row_number() over(partition by opioid_id order by opioid_id desc) as row_id
     from {{ ref('stg_opioid_county_state') }}
 )
 
@@ -13,7 +14,6 @@ select
     o.year,
     o.geo_level,
     o.geo_code,
-    o.state,
     o.plan_type,
     o.opioid_claim_count,
     o.overall_claim_count,
@@ -27,3 +27,4 @@ from {{ ref('stg_opioids') }} as o
 left join 
     county_names as cn 
 on o.opioid_id = cn.opioid_id
+and cn.row_id = 1
