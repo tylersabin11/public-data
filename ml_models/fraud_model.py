85ea37c32b2a6ed86a16fd7019159dbed6f41f96
Tyler Sabin <124528677+tylersabin11@users.noreply.github.com>
2025-07-22 17:01:13 -0500

    Cont. work for model

--- ml_models/fraud_model.py
+++ ml_models/fraud_model.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import pandas as pd 
 from google.cloud import bigquery
-from sklearn.model_selection import train_test_split
 import os 
 
 #Set project root
@@ -16,17 +15,27 @@ project_id = 'opportune-chess-463416-e7'
 dataset_id = 'intermediate'
 
 #Table information
-test_table_name = 'int_test_healthcare_fraud'
 train_table_name = 'int_train_healthcare_fraud'
+test_table_name = 'int_test_healthcare_fraud'
 
 #Set table path to variable
-test_table = f"{project_id}.{dataset_id}.{test_table_name}"
 train_table = f"{project_id}.{dataset_id}.{train_table_name}"
+test_table = f"{project_id}.{dataset_id}.{test_table_name}"
 
 #Create a query string
-test_query = f"SELECT * FROM {test_table}"
 train_query = f"SELECT * FROM {train_table}"
+test_query = f"SELECT * FROM {test_table}"
 
 # Run query and load to DataFrame
-test_df = client.query(test_query).to_dataframe()
 train_df = client.query(train_query).to_dataframe()
+test_df = client.query(test_query).to_dataframe()
+
+#Add potential_fraud to test
+test_df.insert(18, "potential_fraud", np.nan)
+
+#Test train split
+X_train = train_df.drop(columns=["potential_fraud"])
+X_test = test_df.drop(columns=["potential_fraud"])
+
+y_train = train_df["potential_fraud"]
+y_test = test_df["potential_fraud"]
